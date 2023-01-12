189b0d015c3c8e4759d6f509354309b96bdf0265
antonioggt <antonioggt85@gmail.com>
2023-01-12 10:10:50 -0300

    entrega o requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../redux/actions';
+
+const MagicNum = 6;
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  handleChanges = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
+      const { email, password } = this.state;
+
+      const isValid = password.length < MagicNum
+      || !email.includes('@', 1)
+      || !email.includes('.com', 1);
+      console.log(isValid);
+
+      this.setState({
+        isDisabled: isValid,
+      });
+    });
+  };
+
+  handleLoginBtn = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+
+    dispatch(login(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          name="email"
+          type="email"
+          placeholder="Digite seu E-mail aqui"
+          onChange={ this.handleChanges }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={ this.handleChanges }
+        />
+        <button
+          type="button"
+          onClick={ this.handleLoginBtn }
+          disabled={ isDisabled }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
