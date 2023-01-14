8eab64a630da7969eb172ecf015d5f7ef7b67808
antonioggt <antonioggt85@gmail.com>
2023-01-14 16:35:26 -0300

    entrega o requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th scope="col">Descrição</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Método de pagamento</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Moeda</th>
+          <th scope="col">Câmbio utilizado</th>
+          <th scope="col">Valor convertido</th>
+          <th scope="col">Moeda de conversão</th>
+          <th scope="col">Editar/Excluir</th>
+        </tr>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
